6adefdc8108661fb202dc0c52933d2dcffef227f
Diego Franco <diegotfranco@gmail.com>
2022-05-06 15:52:27 -0400

    preparando para o js

---
+++ login.js
@@ -0,0 +1,121 @@
+
+// implementação
+
+// ..:: Para conhecimento ::..
+
+// Local: Nome do Sistema - Módulo e Menu relacionado
+
+// Funcionalidade: informar qual a funcionalidade da implementação
+// Texto Adicional: pode ser informado qualquer coisa referente à implementação
+
+// SENDO nós identificamos o perfil do usuário que vai usar a funcionalidade
+// POSSO a funcionalidade em si que precisa ser desenvolvida
+// PARA QUE a motivação da funcionalidade, incluindo o valor de negócio.
+
+// Cenário 1 - Nome do Cenário
+// Dado que para indicar o cenário atual
+// Quando para indicar a ação do usuário
+// Então para indicar como o software vai reagir
+// E/OU para criar cenários de teste ainda mais ricos.
+
+
+// Critérios de Aceitação
+
+
+// Bugs
+
+// ..:: Para conhecimento ::..
+
+
+// LOCAL: Nome do Sistema - Módulo e Menu relacionado
+
+// VERSÃO: Identificar em que versão do sistema envolvido o problema pode ser repetido. Importante identificar se o problema pode ser repetido na última versão.
+
+// PRÉ-CONDIÇÕES:
+// • Identifique o que deve estar configurado no ambiente para
+// que o problema possa ser repetido;
+// • Pode ser uma lista de configurações a serem marcadas;
+// • Ou simplesmente a indicação de que uma base de dados específica
+// deve ser usada.
+
+// PASSOS PARA REPRODUÇÃO DO ERRO:
+// 1) Monte uma lista indicando os passos que devem ser realizados
+// para repetir o erro;
+// 2) Você pode ser específico e identificar o que deve ser preenchido
+// em cada campo;
+// 3) Principalmente se uma base de dados específica está sendo usada
+// para trabalhar;
+// 4) Deve ser possível para qualquer pessoa repetir o erro lendo esta
+// lista de passos.
+
+// ERRO: Mostrar o erro que está acontecendo. Pode ser com uma identificação do que está acontecendo de errado - e muito importante: mostrar contexto de negócio identificando porque a situação atual é um erro.
+
+// SITUAÇÃO DESEJADA: Descreva a situação que o sistema vai mostrar, identifique configurações que não estão sendo consideradas, mostre o que deve ser modificado pensando em regras de negócio para resolver a situação.
+
+
+// Critérios de Aceitação
+
+
+
+
+//------------------------------------------------------------------------------------------------------//
+
+
+
+
+
+// ..:: Para conhecimento ::..
+
+// Local: GESCON > Login
+
+// Funcionalidade: tela de login para acesso ao sistema
+// Texto Adicional: a tela deve conter as logos do GESCON e da Nota Control, campos para a entrada do CPF e senha, um botão de entrar e um link para redefinir a senha.
+
+// SENDO contribuintes e funcionários da prefeitura
+// POSSO acessar a tela de login
+// PARA QUE possam abrir ou gerenciar os processos abertos dentro do sistema
+
+// Cenário 1 – Sucesso
+// Dado que o usuário inseriu seu CPF e senha corretamente nos respectivos campos
+// Quando clicar no botão entrar 
+// Então deverá ser redirecionado a página principal (dashboard)
+
+// Cenário 2 – Falha CPF
+// Dado que o usuário inseriu seu CPF incorretamente no respectivo campo
+// Quando terminar de escrever
+// Então o campo deverá ser limpo, sua borda deverá assumir a cor vermelha e sua borda deverá ser aumentada para 2px
+
+// Cenário 3 – Falha Senha
+// Dado que o usuário inseriu sua senha incorretamente no respectivo campo
+// Quando clicar no botão entrar
+// Então uma snackbar deverá aparecer com fundo em tom mais claro de vermelho sinalizando o erro, sua borda deverá assumir a cor vermelha e sua borda deverá ser aumentada para 2px
+
+// Critérios de Aceitação:
+
+// 1 – O campo CPF deverá possuir mascara para facilitar a leitura
+// 2 – O campo CPF deverá validar o texto inserido e limitar a 11 caracteres
+// 3 – O campo senha não deve mostrar os caracteres digitados e limitar a 20 caracteres
+// 4 – O layout deverá seguir o modelo abaixo
+
+
+
+
+// ..:: Para conhecimento ::..
+
+
+// LOCAL: aplicativo Mercadinho Me Contrata > Sacola
+
+// VERSÃO: 0.1
+
+// PRÉ-CONDIÇÕES:
+// • ter feito o login;
+// • ter adicionado um ou mais produtos à sua sacola;
+
+// PASSOS PARA REPRODUÇÃO DO ERRO:
+// 1) clique em sacola no menu de opções;
+// 2) clique em pagar;
+
+// ERRO: o modal de aviso informa que o produto foi adicionado ao carrinho com sucesso e o usuário não é redirecionado para a página principal.
+
+// SITUAÇÃO DESEJADA: o modal deve informar que a compra foi realizada com sucesso e o usuário é redirecionado para a página principal
+
