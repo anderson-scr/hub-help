9abd0817fabe508466bf0d98ed77ec55e186c3ad
Diego Franco <diegotfranco@gmail.com>
2022-05-10 08:26:46 -0400

    adc o aviso de confirmar saida

--- login.js
+++ login.js
@@ -1,4 +1,4 @@
-function openModal(el, ev)
+function openModal(el)
 {
 	img = document.querySelector('#img-modal');
 	titulo = document.querySelector('#titulo-modal');
@@ -6,13 +6,13 @@ function openModal(el, ev)
 
 	if(el.id === 'login')
 	{
-		img.src = "img/check_circle_black_48dp.svg";
+		img.src = "img/check_circle_blue_48dp.svg";
 		img.alt = "simbolo de sucesso"
 		titulo.innerHTML = "Conta criada com sucesso!";		
 	}
 	else if(el.id === 'enviar-email')
 	{
-		img.src = "img/check_circle_black_48dp.svg";
+		img.src = "img/check_circle_blue_48dp.svg";
 		img.alt = "simbolo de sucesso"
 		titulo.innerHTML = "Senha enviada com sucesso!";
 		console.log(`Uma senha temporária foi enviada para o e-mail ${el.previousElementSibling.value}`);
@@ -20,21 +20,27 @@ function openModal(el, ev)
 	}
 	else if(el.id === 'alterar-senha')
 	{
-		img.src = "img/check_circle_black_48dp.svg";
+		img.src = "img/check_circle_blue_48dp.svg";
 		img.alt = "simbolo de sucesso"
 		titulo.innerHTML = "A alteração de senha foi realizada com sucesso!";
 	}
+	else if(el.id === 'confirmar-saida')
+	{
+		img.src = "img/help_blue_48dp.svg";
+		img.alt = "simbolo de duvida"
+		titulo.innerHTML = "Você tem certeza que gostaria de sair?";	
+	}
 
-	btn = document.querySelector(".modal-bg")
-	btn.style.visibility = 'visible';
-	btn.style.opacity = '1';
+	modal = document.querySelector(".modal-bg")
+	modal.style.visibility = 'visible';
+	modal.style.opacity = '1';
 }
 
 function closeModal()
 {
-	btn = document.querySelector(".modal-bg")
-	btn.style.visibility = 'hidden';
-	btn.style.opacity = '0';
+	modal = document.querySelector(".modal-bg")
+	modal.style.visibility = 'hidden';
+	modal.style.opacity = '0';
 }
 
 function trocaAba()
@@ -70,7 +76,6 @@ function trocaAba()
 			atual.parentElement.parentElement.style.height = "55%";
 			atual.parentElement.parentElement.style.borderRadius = "4.8%";
 			atual.parentElement.parentElement.innerHTML = data;
-			// setaModal();
 		});
 
 	}
